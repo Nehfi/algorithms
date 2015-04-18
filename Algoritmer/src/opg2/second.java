4bbed14e50e23c5fe1d3ef5366a02bdfd5eb0016
nehfi <nehfi@Alfred>
2015-04-18 16:55:29 +0200

    liiidt videre.. næsten done now :)

--- Algoritmer/src/opg2/second.java
+++ Algoritmer/src/opg2/second.java
@@ -8,52 +8,98 @@ import java.util.Collections;
 
 public class main {
 
-	/**
-	 * For den obligatoriske opgave i algoritmer
-	 * Af Amal, Kim og Konstantin
-	 */
+	static String[] persons;
+	static boolean[][] friends;
+
 	public static void main(String[] args) throws IOException {
 //		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedReader reader = new BufferedReader(new FileReader("input2"));
-		
+
 		String line;
-		String group;
-		ArrayList<String> names = new ArrayList<String>();
-		ArrayList<Integer> friendships = new ArrayList<Integer>();
-		int n = 0;
-		
+		String group = null;
+
+		int E = 0;
+		ArrayList<Integer> gr = new ArrayList<Integer>();
+		String[] q;
+
 		while((line=reader.readLine()) != null){
-			if (line.trim().compareTo("taetvenskab") == 0) {
-				group = line;
-				System.out.println(line);
+			if (line.trim().startsWith("taetvenskab") == true){
+				group = line.substring(11).trim();
 				reader.close();
 				break;
-			}else if(line.trim().compareTo("[a-zA-Z]") == 0){
-				Collections.addAll(names, line.split("\\s+"));
-			}else{
-				
 			}
-			
-			n++;
+			else if(line.trim().substring(0,1).matches("[a-zA-Z]")){
+				int V = countwords(line);
+				System.out.println("V: " + V);
+				persons = new String[V];
+				friends = new boolean[V][V];
+
+				for(int i = 0; i<V; i++){
+					for(int j=0; j<i; j++){
+						friends[i][j] = false;
+//						System.out.println("Freinds i,j: " + friends[i][j]);
+					}
+				}
+			}
+			else if(line.trim().substring(0,1).matches("[0-9]")){
+				int p1 = Integer.parseInt(line.trim().substring(0, 1));
+				int p2 = Integer.parseInt(line.trim().substring(2));
+				addEdge(p1, p2);
+				E++;
+			}
 		}
 
-		System.out.println(line);
-		
+		boolean isTight = tightFriends(group);
+		System.out.println("Be tight? " + isTight);
 	}
 
-	public boolean tightFriends() {
+	public static boolean tightFriends(String str) {
 		boolean bool = false;
+
+		String[] digitwords = str.split("\\s+");
+		int[] result = new int[digitwords.length];
+		for (int i = 0; i < result.length; i++) {
+		    result[i] = Integer.parseInt(digitwords[i]);
+		    System.out.println("Res :" + result[i]);
+		}
+
+		
 		
 		return bool;
 	}
-	
-	public void addEdge(int p1, int p2){
-		
+
+	public static void addEdge(int p1, int p2){
+		friends[p1][p2] = true;
+		friends[p2][p1] = true;
+		System.out.println("Venskab " + p1 + " og " + p2 + " er: " + friends[p2][p1]);
 	}
 
-	public boolean isEdge(int p1, int p2){
+	public static boolean isEdge(int p1, int p2){
 		boolean bool = false;
-		
+		bool = friends[p1][p2];
+
+		System.out.println("IsEdge?: " + bool);
+
 		return bool;
+	}
+
+	public static int countwords(String str){
+		boolean word = false;
+		int wordCount = 0;
+		int  endOfLine = str.length () -1;
+
+		for (int i =0; i<str.length(); i++){
+			if (Character.isLetter(str.charAt(i)) && i != endOfLine){
+				word=true;
+			}
+			else if (!Character.isLetter(str.charAt(i)) && word){
+				wordCount++;
+				word=false;
+
+			} else if (Character.isLetter(str.charAt(i)) && i==endOfLine){
+				wordCount++;
+			}
+		}
+		return wordCount;
 	}
 }
